795568b7c7a9df7378655f8de1549e396b90e80e
Michael Pope <map7777@gmail.com>
2024-04-04 21:38:02 +1100

    Add edge cases

--- add_test.rb
+++ add_test.rb
@@ -1,19 +1,44 @@
 require 'test-unit'
 
 def add(a,b)
+  
+  if a == nil
+    a = 0
+  elsif b.nil?
+    b = 0
+  end
+
+  if a.class != Integer or b.class != Integer
+    raise TypeError, "No bloody strings thanks"
+  end
+  
   a + b
 end
 
 class AddTest < Test::Unit::TestCase
   
+  # Happy path
   def test_add__number
     result = add(1,1)
     assert_equal 2,result
   end
 
+  # Edge cases
   def test_add__string
-    assert_raise TypeError do
+    assert_raise_with_message(TypeError,"No bloody strings thanks") do
       add(1,"foo")
     end
   end
+
+  def test_add__nil
+    result = add(1,nil)
+    assert_equal 1,result
+  end
+
+  def test_add__nil_string
+    assert_raise_with_message(TypeError,"No bloody strings thanks") do
+      add(nil,"foo")
+    end
+  end
+  
 end
