e727a16b313b7e832796357555ac71898835839e
Michael Pope <map7777@gmail.com>
2024-04-04 23:00:45 +1100

    Setup a test for adding a bookshelf

---
+++ add_bookshelf_e2e.rb
@@ -0,0 +1,11 @@
+require_relative 'test_helper'
+require_relative 'bookshelf.rb'
+
+class AddBookshelf < Test::Unit::TestCase
+  include Capybara::DSL
+  include TestHelper
+
+  def test_adding_bookshelf
+    visit "/"
+  end
+end  
