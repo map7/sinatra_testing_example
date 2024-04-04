7f3e0fca398e529abfec5e1756289242aa6aa1b4
Michael Pope <map7777@gmail.com>
2024-04-04 23:53:55 +1100

    Add a full system end to end test for adding a bookshelf

--- add_bookshelf_e2e.rb
+++ add_bookshelf_e2e.rb
@@ -7,10 +7,24 @@ class AddBookshelf < Test::Unit::TestCase
 
   def test_adding_bookshelf
     visit "/"
+    assert_current_path "/"
     assert page.has_content?("Bookshelves")
-
     click_on "new_bookshelf"
+
+    # On Add page
+    assert_current_path "/add"
     assert page.has_no_content?("Bookshelves")
     assert page.has_content?("Add Bookshelf")
+
+    fill_in "name", with: "Fred"
+    fill_in "location", with: "Lounge"
+    click_on "submit"
+
+    # Back on the index page
+    assert_current_path "/"
+    assert page.has_content?("Bookshelves")
+    assert page.has_content?("Fred")
+    assert page.has_content?("Lounge")
+    
   end
 end  
--- bookshelf.rb
+++ bookshelf.rb
@@ -1,12 +1,32 @@
 require 'sinatra/base'
+require 'json'
+require 'debug'
 
 class BookshelfApp < Sinatra::Base
-  get '/' do 
+  get '/' do
+    
+    File.open("bookshelves.json") do |file|
+      @hash=JSON.parse(file.read)
+    end
+
+    @name = @hash.dig("name")
+    @location = @hash.dig("location")
     erb :home
   end
 
   get '/add' do 
     erb :add
+  end
+
+  post '/add' do
+    hash = {name: params[:name],
+            location: params[:location] }
+
+    File.open("bookshelves.json","w") do |file|
+      file.write JSON.pretty_generate(hash)
+    end
+
+    redirect "/"
   end
 end
 
