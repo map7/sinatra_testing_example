02855f14a44a5ff59f68c9de5f126a91afc6e5e0
Michael Pope <map7777@gmail.com>
2024-04-04 21:24:26 +1100

    Add code

--- add_test.rb
+++ add_test.rb
@@ -1,9 +1,19 @@
 require 'test-unit'
 
-class AddTest < Test::Unit::TestCase
+def add(a,b)
+  a + b
+end
 
-  def test_add
+class AddTest < Test::Unit::TestCase
+  
+  def test_add__number
     result = add(1,1)
     assert_equal 2,result
+  end
+
+  def test_add__string
+    assert_raise TypeError do
+      add(1,"foo")
+    end
   end
 end
